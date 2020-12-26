7b195254501dfd8beae121c096070e62a27cb8eb
RayK <zrztt@outlook.com>
2020-12-26 16:29:35 +0800

    feat: 连接图形前端和api

--- ui/window_main.py
+++ ui/window_main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from weather.api_master import ApiMaster
+from utils.config import Config
 from PIL import Image, ImageTk
 
 
@@ -6,19 +8,22 @@ class WindowMain(tk.Tk):
     def __init__(self) -> None:
         super().__init__()
 
-        self.city_text = tk.StringVar()
-        self.time_text = tk.StringVar()
-        self.description_text = tk.StringVar()
-        self.temperature_text = tk.StringVar()
-        self.rain_text = tk.StringVar()
-        self.wet_text = tk.StringVar()
-        self.windspeed_text = tk.StringVar()
-        self.img_main = None
+        self.__city_text = tk.StringVar()
+        self.__time_text = tk.StringVar()
+        self.__description_text = tk.StringVar()
+        self.__temperature_text = tk.StringVar()
+        self.__rain_text = tk.StringVar()
+        self.__wet_text = tk.StringVar()
+        self.__windspeed_text = tk.StringVar()
+        self.__img_main = None
 
-        self.testText()
-        self.setup_ui()
+        self.__citys = Config().get()['citys']['list']
+        self.__city_show = 0
+        self.__init_text()
+        self.__setup_ui()
+        self.__fresh_ui()
 
-    def setup_ui(self):
+    def __setup_ui(self):
         self.title = ""
         # self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
@@ -26,15 +31,15 @@ class WindowMain(tk.Tk):
         # 城市信息Frame
         city_info_frame = tk.Frame(self, bg='red')
         city_label = tk.Label(city_info_frame,
-                              textvariable=self.city_text,
+                              textvariable=self.__city_text,
                               font=(None, 20))
         city_label.grid(row=0, column=0, padx=20, pady=10, sticky=tk.NW)
         time_label = tk.Label(city_info_frame,
-                              textvariable=self.time_text,
+                              textvariable=self.__time_text,
                               font=(None, 10))
         time_label.grid(row=1, column=0, padx=20, pady=5, sticky=tk.NW)
         description_label = tk.Label(city_info_frame,
-                                     textvariable=self.description_text,
+                                     textvariable=self.__description_text,
                                      font=(None, 10))
         description_label.grid(row=2, column=0, padx=20, pady=5, sticky=tk.NW)
         # 城市信息布局
@@ -44,10 +49,10 @@ class WindowMain(tk.Tk):
         weather_info_frame = tk.Frame(self, bg='blue')
         # 气温Frame
         temperature_frame = tk.Frame(weather_info_frame)
-        img_label = tk.Label(temperature_frame, image=self.img_main)
+        img_label = tk.Label(temperature_frame, image=self.__img_main)
         img_label.grid(row=0, column=0)
         temperature_label = tk.Label(temperature_frame,
-                                     textvariable=self.temperature_text,
+                                     textvariable=self.__temperature_text,
                                      font=(None, 50))
         temperature_label.grid(row=0, column=1, sticky=tk.W)
         # 气温布局
@@ -59,12 +64,13 @@ class WindowMain(tk.Tk):
 
         # 更多信息Frame
         more_message_frame = tk.Frame(weather_info_frame)
-        rain_label = tk.Label(more_message_frame, textvariable=self.rain_text)
+        rain_label = tk.Label(more_message_frame,
+                              textvariable=self.__rain_text)
         rain_label.grid(row=0, column=0, sticky=tk.W)
-        wet_label = tk.Label(more_message_frame, textvariable=self.wet_text)
+        wet_label = tk.Label(more_message_frame, textvariable=self.__wet_text)
         wet_label.grid(row=1, column=0, sticky=tk.W)
         windspeed_label = tk.Label(more_message_frame,
-                                   textvariable=self.windspeed_text)
+                                   textvariable=self.__windspeed_text)
         windspeed_label.grid(row=2, column=0, sticky=tk.W)
         # 更多信息布局
         more_message_frame.grid(row=0,
@@ -83,13 +89,30 @@ class WindowMain(tk.Tk):
 
         weekly_weather_frame.grid(row=3, column=0, sticky=tk.S)
 
-    def testText(self):
-        self.city_text.set("沈阳")
-        self.time_text.set("2010/12/19 16:11")
-        self.description_text.set("晴转多云")
-        self.temperature_text.set("19°C")
-        self.rain_text.set("降水概率：0%")
-        self.wet_text.set("湿度：45%")
-        self.windspeed_text.set("风速：45km/s")
-        image = Image.open("res/img/sunny.png").resize((100, 100))
-        self.img_main = ImageTk.PhotoImage(image)
+    def __init_text(self):
+        self.__city_text.set("——")
+        self.__time_text.set("————/——/—— ——:——")
+        self.__description_text.set("——")
+        self.__temperature_text.set("——°C")
+        self.__rain_text.set("降水概率：——%")
+        self.__wet_text.set("湿度：——%")
+        self.__windspeed_text.set("风速：——km/s")
+        image = Image.open("res/img/wutu.png").resize((100, 100))
+        self.__img_main = ImageTk.PhotoImage(image)
+
+    def __fresh_ui(self):
+        if self.__citys:
+            api = ApiMaster(self.__citys[self.__city_show])
+            api.fresh_weather()
+            curr = api.get_current_weather()
+            # week = api.get_weekly_weather()
+            self.__city_text.set(self.__citys[self.__city_show])
+            self.__time_text.set(curr["update_time"])
+            self.__description_text.set(curr["wea"])
+            self.__temperature_text.set("{}°C".format(curr["tem"]))
+            self.__rain_text.set("空气质量：{}".format(curr["air"]))
+            self.__wet_text.set("湿度：{}".format(curr["humidity"]))
+            self.__windspeed_text.set("风速：{}".format(curr["win_meter"]))
+            image = Image.open("res/img/{}.png".format(
+                curr["wea_img"])).resize((100, 100))
+            self.__img_main = ImageTk.PhotoImage(image)
--- weather/api_master.py
+++ weather/api_master.py
@@ -4,11 +4,24 @@ from utils.config import Config
 
 
 class ApiMaster:
-    def __init__(self) -> None:
-        self.__config = Config().get()['weather-api']
-        self.__current_weather = None
-        self.__weekly_weather = None
-        self.city = '沈阳'
+    # 单例模式
+    __instance = None
+    __first_init = False
+
+    def __new__(cls, *args, **kw):
+        if cls.__instance is None:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
+
+    def __init__(self, city: str = None, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        if not self.__first_init:
+            self.__first_init = True
+            self.__config = Config().get()['weather-api']
+            self.__current_weather = None
+            self.__weekly_weather = None
+        if city is not None:
+            self.city = city
 
     def fresh_weather(self):
         self.__current_weather = CurrentWeather(self.__config, self.city).get()
